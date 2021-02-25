5befdb5cdf2cade6151ebe8ecb7b53d569bb8821
Leila Learning <4everwith@gmail.com>
2021-02-25 16:34:26 -0700

    digging deeper into TS

---
+++ dist/app.js
@@ -0,0 +1,34 @@
+"use strict";
+//arrow functions
+const add = (a, b) => a + b;
+const printOutput = output => console.log(output);
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
+printOutput(add(3, 8));
+//set default arguments
+const summ = (x, y = 1) => x + y;
+const printOutput1 = output => console.log(output);
+printOutput1(summ(7));
+//spread operator and rest parameters
+const hobbies = ["Cooking", "Walking", "Reading"];
+const activeHobbies = ["Hiking"];
+activeHobbies.push(...hobbies);
+const person = {
+    FirstName: "Helga",
+    age: 23
+};
+const copyOfPerson = Object.assign({}, person);
+const add1 = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add1(2, 8, 15, 7, 30);
+console.log(addedNumbers);
+//array and object destructuring
+const [hobby1, hobby2, ...restHobbies] = hobbies;
+console.log(hobby1, hobby2);
+const { FirstName: userName, age } = person;
+console.log(userName, age);
---
+++ src/arraysAndRetrievinginfo.ts
@@ -0,0 +1,58 @@
+//arrow functions
+
+const add = (a: number, b: number) => a+b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', event =>console.log(event));
+}
+
+printOutput(add(3,8));
+
+//set default arguments
+
+const summ = (x: number, y: number = 1) => x+y;    //y is set by default
+
+const printOutput1: (x: number | string) => void = output => console.log(output);
+
+
+printOutput1(summ(7));
+
+//spread operator and rest parameters
+
+const hobbies = ["Cooking", "Walking", "Reading"];
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+    FirstName: "Helga",
+    age: 23
+};
+
+const copyOfPerson = {...person};
+
+
+
+const add1 = (...numbers: number []) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+
+};
+
+const addedNumbers = add1(2,8,15,7,30);
+console.log(addedNumbers);
+
+//array and object destructuring
+
+const [hobby1, hobby2, ...restHobbies] = hobbies;
+
+console.log(hobby1, hobby2);
+
+const {FirstName: userName, age} = person;
+
+console.log(userName, age);
