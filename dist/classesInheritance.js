1cfe7acd8754dbaf06512f2ae04fbf412ee9cf26
Leila Learning <4everwith@gmail.com>
2021-03-03 16:37:52 -0700

    inheritance and more

--- dist/classesInheritance.js
+++ dist/classesInheritance.js
@@ -5,9 +5,13 @@ class Department {
         this.name = name;
         //private name: string;
         //private id: number;
-        this.employees = [];
+        this.employees = []; //protected instead of private so that it can be accessable and overwritten in inherited classes
         //this.name =n;
         //this.id = id
+        //console.log(Department.fiscalYear); //way to access static method from inside of non-static function
+    }
+    static createEmployee(name) {
+        return { name: name };
     }
     describe() {
         console.log('Department: ' + this.id + '.' + this.name);
@@ -20,17 +24,40 @@ class Department {
         console.log(this.employees);
     }
 }
+Department.fiscalYear = 2020;
 class ITDepartment extends Department {
     constructor(id, admins) {
         super(id, 'IT'); //calls the constractor of the base class (of Department). At least one super call must be in the function body of derived class (ITDepartment in this case)
         this.admins = admins;
-        this.admins = admins;
+        // this.admins = admins; //this is always used after we use super
     }
 }
 class AccountingDepartment extends Department {
     constructor(id, payrolls) {
         super(id, "Acounting");
         this.payrolls = payrolls;
+        this.lastPayroll = payrolls[0];
+    }
+    get mostRecentPayroll() {
+        if (this.lastPayroll) { //added a check if lastPayroll has value
+            return this.lastPayroll;
+        }
+        throw new Error('No payroll found');
+    }
+    set mostRecentPayroll(value) {
+        if (!value) { //check if there is value (no value)
+            throw new Error("Sorry, nothing found");
+        }
+        this.addPayrolls(value);
+    }
+    describe() {
+        console.log("Department: Accounting. ID: " + this.id);
+    }
+    addEmployee(name) {
+        if (name === 'John') {
+            return;
+        }
+        this.employees.push(name);
     }
     addPayrolls(numbers) {
         this.payrolls.push(numbers);
@@ -39,17 +66,24 @@ class AccountingDepartment extends Department {
         console.log(this.payrolls);
     }
 }
-const accounting = new AccountingDepartment(5, []);
+const employee1 = Department.createEmployee("Andrew");
+console.log(employee1, Department.fiscalYear);
 const it = new ITDepartment(3, ['John, Mary, Edward']);
+it.addEmployee('John');
+it.addEmployee('Mary');
+it.addEmployee('Edward');
+it.describe();
+it.printEmployeeInfo();
+console.log(it);
+const accounting = new AccountingDepartment(5, []);
+accounting.describe();
 accounting.addEmployee('Eva');
 accounting.addEmployee('Max');
 accounting.addEmployee('Anna');
-it.addEmployee('John');
-it.addEmployee('Mary');
-it.addEmployee('Adward');
-accounting.describe();
+accounting.addEmployee('Kathleen');
 accounting.printEmployeeInfo();
-it.describe();
-it.printEmployeeInfo();
-accounting.addPayrolls(35);
-accounting.printPayrolls();
+//accounting.addPayrolls(35);
+//accounting.mostRecentPayroll = 112;
+//accounting.printPayrolls();
+console.log(accounting);
+console.log(accounting.mostRecentPayroll);
--- src/classesInheritance.ts
+++ src/classesInheritance.ts
@@ -1,11 +1,17 @@
 class Department {
+
+    static fiscalYear = 2020;
     //private name: string;
     //private id: number;
-    private employees:string[] = [];
+    protected employees:string[] = []; //protected instead of private so that it can be accessable and overwritten in inherited classes
 
-    constructor(private readonly id: number, public name: string) {
+    constructor(protected readonly id: number, public name: string) { //instead of writing 2 and 3 row, we can initialize those in constructor
         //this.name =n;
         //this.id = id
+        //console.log(Department.fiscalYear); //way to access static method from inside of non-static function
+    }
+    static createEmployee(name: string){
+        return{name:name};
     }
 
     describe(this: Department) {
@@ -22,16 +28,46 @@ class Department {
     }
 }
 
+
+
 class ITDepartment extends Department { //class ITDepartment inherits everything from class Department
         constructor(id: number, public admins: string[]) { //we can create our own constractor for class ITDepartment that shows what exactly we want to inherit
          super(id, 'IT'); //calls the constractor of the base class (of Department). At least one super call must be in the function body of derived class (ITDepartment in this case)
-         this.admins = admins;
+        // this.admins = admins; //this is always used after we use super
     }
 }
 
 class AccountingDepartment extends Department {
+    private lastPayroll: number;
+
+    get mostRecentPayroll(){ //use get to retrieve a value of unaccessable (private) property
+        if (this.lastPayroll) { //added a check if lastPayroll has value
+            return this.lastPayroll;
+        }
+        throw new Error('No payroll found');
+    }
+
+    set mostRecentPayroll(value: number) {
+        if (!value) { //check if there is value (no value)
+            throw new Error ("Sorry, nothing found");
+        }
+        this.addPayrolls(value);
+    }
+
     constructor(id: number, private payrolls: number[]) {
         super(id, "Acounting");
+        this.lastPayroll = payrolls[0]
+    }
+
+    describe() {
+        console.log("Department: Accounting. ID: " +this.id);
+    }
+
+    addEmployee (name: string) {
+        if (name === 'John') {
+           return;
+        }
+        this.employees.push(name);
     }
 
     addPayrolls(numbers: number) {
@@ -43,24 +79,36 @@ class AccountingDepartment extends Department {
     }
 }
 
-const accounting = new AccountingDepartment(5, []);
+const employee1 = Department.createEmployee ("Andrew");
+console.log(employee1, Department.fiscalYear);
+
 const it = new ITDepartment(3, ['John, Mary, Edward']);
-
-
-accounting.addEmployee('Eva');
-accounting.addEmployee('Max');
-accounting.addEmployee('Anna');
 
 it.addEmployee('John');
 it.addEmployee('Mary');
-it.addEmployee('Adward');
+it.addEmployee('Edward');
 
-accounting.describe(); 
-accounting.printEmployeeInfo();
 
 it.describe();
 it.printEmployeeInfo();
 
-accounting.addPayrolls(35);
-accounting.printPayrolls();
+console.log(it);
+
+const accounting = new AccountingDepartment(5, []);
+
+accounting.describe();
+
+accounting.addEmployee('Eva');
+accounting.addEmployee('Max');
+accounting.addEmployee('Anna');
+accounting.addEmployee('Kathleen');
+
+accounting.printEmployeeInfo();
+
+//accounting.addPayrolls(35);
+//accounting.mostRecentPayroll = 112;
+//accounting.printPayrolls();
+
+console.log(accounting);
+console.log(accounting.mostRecentPayroll);
 
