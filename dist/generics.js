66b04c46b2361dede8333a784aa2fe3ac37ec55b
Night-Cheza <leila.kassi@outlook.com>
2021-03-10 17:43:40 -0700

    generics

--- dist/generics.js
+++ dist/generics.js
@@ -5,3 +5,14 @@ function merge(objA, objB) {
 }
 const merjedObj = merge({ name: "Vikki", hobbies: ["Reading"] }, { age: 25 });
 console.log(merjedObj.age);
+function CountAndDiscribe(element) {
+    let descriptionText = "Got no value";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element";
+    }
+    else if (element.length > 1) {
+        descriptionText = "Got " + element.length + "elements";
+    }
+    return [element, descriptionText];
+}
+console.log(CountAndDiscribe("Hi there"));
--- src/generics.ts
+++ src/generics.ts
@@ -6,3 +6,19 @@ function merge<T extends object, U extends object>(objA: T, objB: U) { //generic
 
 const merjedObj = merge({name: "Vikki", hobbies: ["Reading"]}, {age:25});
 console.log(merjedObj.age);
+
+interface Lengthy {
+    length: number;
+}
+
+function CountAndDiscribe<T extends Lengthy>(element: T){
+    let descriptionText = "Got no value";
+    if (element.length === 1){
+        descriptionText = "Got 1 element";
+    } else if (element.length > 1){
+        descriptionText = "Got " + element.length + "elements";
+    }
+    return[element, descriptionText];
+}
+
+console.log(CountAndDiscribe ("Hi there"));
