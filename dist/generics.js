fe80a5476ba713cd6163555c28a105098a2e3fee
Night-Cheza <leila.kassi@outlook.com>
2021-03-11 18:11:44 -0700

    generics

--- dist/generics.js
+++ dist/generics.js
@@ -1,5 +1,5 @@
 "use strict";
-// Generics and constraints
+// Generic functions and constraints
 function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
@@ -16,3 +16,28 @@ function CountAndDiscribe(element) {
     return [element, descriptionText];
 }
 console.log(CountAndDiscribe("Hi there"));
+//keyof
+function extractAndConvert(obj, key) {
+    return "Value: " + obj[key]; //[] - to access property on object - js
+}
+extractAndConvert({ name: "Max" }, "name");
+//Generic classes
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem("Gorik");
+textStorage.addItem("Jinn");
+textStorage.removeItem("Gorik");
+console.log(textStorage.getItems());
--- src/generics.ts
+++ src/generics.ts
@@ -1,4 +1,4 @@
-// Generics and constraints
+// Generic functions and constraints
 
 function merge<T extends object, U extends object>(objA: T, objB: U) { //generic function with constraints for T and U input
     return Object.assign(objA, objB); 
@@ -11,7 +11,7 @@ interface Lengthy {
     length: number;
 }
 
-function CountAndDiscribe<T extends Lengthy>(element: T){
+function CountAndDiscribe<T extends Lengthy>(element: T){ //constraint that we need an object that has length property
     let descriptionText = "Got no value";
     if (element.length === 1){
         descriptionText = "Got 1 element";
@@ -22,3 +22,37 @@ function CountAndDiscribe<T extends Lengthy>(element: T){
 }
 
 console.log(CountAndDiscribe ("Hi there"));
+
+//keyof
+
+function extractAndConvert<T extends object, U extends keyof T> (obj: T, key: U){
+    return "Value: " + obj[key]; //[] - to access property on object - js
+}
+
+extractAndConvert({name:"Max"}, "name");
+
+//Generic classes
+
+class DataStorage <T> { //generic class we can call whenever we need to store any primitive type info
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item:T) {
+        this.data.splice(this.data.indexOf(item), 1); //works only for primitive types, otherwise need to add extra check
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Gorik");
+textStorage.addItem("Jinn");
+textStorage.removeItem("Gorik");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
