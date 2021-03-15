c0b3b140c5df0620c145e86b37d76d86034281ab
Night-Cheza <leila.kassi@outlook.com>
2021-03-15 17:18:19 -0600

    decorators added

--- dist/decorators.js
+++ dist/decorators.js
@@ -1,22 +1,68 @@
 "use strict";
+//decorator
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(target) {
+/* function Logger(target: Function){
     console.log("Loggin...");
     console.log(target);
 }
+
+@Logger
+class Person1{
+    name = "Eva";
+
+    constructor() {
+    console.log("Creating a new person")
+    }
+}
+
+const pers = new Person1();
+
+console.log(pers); */
+//decorator factory
+function Logger(logString) {
+    console.log("LOGGER FACTORY");
+    return function (target) {
+        console.log(logString);
+        console.log(target);
+    };
+}
 let Person1 = class Person1 {
     constructor() {
-        this.name = "Max";
+        this.name = "Eva";
         console.log("Creating a new person");
     }
 };
 Person1 = __decorate([
-    Logger
+    Logger("Loggin person")
 ], Person1);
 const pers = new Person1();
 console.log(pers);
+//decorators and work with HTML
+function WithTemplate(template, hookID) {
+    console.log("TEMPLATE FACTORY");
+    return function (_) {
+        console.log("Rendering template");
+        const hookElement = document.getElementById(hookID);
+        if (hookElement) {
+            hookElement.innerHTML = template;
+        }
+    };
+}
+let Person2 = class Person2 {
+    constructor() {
+        this.name = "May";
+        console.log("Creating a new person");
+    }
+};
+Person2 = __decorate([
+    Logger("Loggin..."),
+    WithTemplate("<h1>A New Person Object</h1>", "app")
+], Person2);
+const pers2 = new Person2();
+console.log(pers2);
+//multiple decorators
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,11 +1,36 @@
-function Logger(target: Function){
+//decorator
+
+/* function Logger(target: Function){
     console.log("Loggin...");
     console.log(target);
 }
 
 @Logger
 class Person1{
-    name = "Max";
+    name = "Eva";
+
+    constructor() {
+    console.log("Creating a new person")
+    }
+}
+
+const pers = new Person1();
+
+console.log(pers); */
+
+//decorator factory
+
+function Logger(logString: string) {
+    console.log ("LOGGER FACTORY");
+    return function(target: Function) { //decorator function
+        console.log(logString);
+        console.log(target);
+    }; 
+}
+
+@Logger ("Loggin person")
+class Person1{
+    name = "Eva";
 
     constructor() {
     console.log("Creating a new person")
@@ -15,3 +40,32 @@ class Person1{
 const pers = new Person1();
 
 console.log(pers);
+
+//decorators and work with HTML
+
+function WithTemplate(template: string, hookID: string) {
+    console.log("TEMPLATE FACTORY");
+    return function(_: Function) { // _ says to ts that I get an argument which I don't need, I'm aware I have it, but I don't use it.
+       console.log("Rendering template");
+       const hookElement = document.getElementById(hookID);
+       if (hookElement) {
+           hookElement.innerHTML = template;
+       }
+    }
+}
+
+@Logger("Loggin...")
+@WithTemplate("<h1>A New Person Object</h1>", "app")
+class Person2{
+    name = "May";
+
+    constructor() {
+    console.log("Creating a new person")
+    }
+}
+
+const pers2 = new Person2();
+
+console.log(pers2);
+
+//multiple decorators
